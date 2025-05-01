6b1c32613f5f8430497f36b6f40188f79630c33a
JTCHE <89811963+JTCHE@users.noreply.github.com>
2025-05-01 21:08:44 +0200

    feat: started to implement search type selection. feature hidden for now as it's non-functional

--- app/(index)/components/input.tsx
+++ app/(index)/components/input.tsx
@@ -3,7 +3,9 @@
 import { Button } from "@/shadcn/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/shadcn/components/ui/form";
 import { Input } from "@/shadcn/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/shadcn/components/ui/select";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -13,8 +15,24 @@ interface SpotifyInputProps {
   loading: boolean;
 }
 
+const searchTypes = {
+  URL: {
+    name: "URL",
+    exampleString: "https://spotify.com/track/7eqoqGkKwgOaWNNHx90uEZ",
+  },
+  ID: {
+    name: "ID",
+    exampleString: "7eqoqGkKwgOaWNNHx90uEZ",
+  },
+  songName: {
+    name: "Song Name",
+    exampleString: "Nights - Frank Ocean",
+  },
+};
+
 // Defining the form's schema
 const formSchema = z.object({
+  searchType: z.string(),
   url: z.string().url().includes("spotify.com/track", {
     message: "Invalid Spotify URL. Please enter a valid Spotify track URL.",
   }),
@@ -26,6 +44,7 @@ export default function SpotifyInput({ loading, onSubmit }: SpotifyInputProps) {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      searchType: Object.keys(searchTypes)[0],
       url: "",
     },
   });
@@ -40,27 +59,60 @@ export default function SpotifyInput({ loading, onSubmit }: SpotifyInputProps) {
     }
   }
 
+  const [isReady, setIsReady] = useState(false); //State to know whether the form can be submitted or not, depending on the search type
+
   return (
-    <div className="w-full">
-      <Form {...form}>
-        <form className="space-y-4" onSubmit={form.handleSubmit(handleSubmit)}>
-          <FormField
-            control={form.control}
-            name="url"
-            render={({ field }) => (
-              <FormItem>
+    <Form {...form}>
+      <form
+        className="space-y-4 w-full"
+        onSubmit={form.handleSubmit(handleSubmit)}
+      >
+        {/* <FormField
+          control={form.control}
+          name="searchType"
+          render={({ field }) => (
+            <FormItem>
+              <Select
+                defaultValue={field.value}
+                onValueChange={field.onChange}
+              >
                 <FormControl>
-                  <Input {...field} placeholder="https://spotify.com/track/7eqoqGkKwgOaWNNHx90uEZ" />
+                  <SelectTrigger className="w-full">
+                    <SelectValue />
+                  </SelectTrigger>
                 </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button disabled={!form.watch("url").includes('spotify.com/track') || loading} type="submit" className="w-full">
-            {loading ? "Loading…" : "Submit"}
-          </Button>
-        </form>
-      </Form>
-    </div>
+                <SelectContent>
+                  {Object.entries(searchTypes).map(([key, value]) => (
+                    <SelectItem value={key}>{value.name}</SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </FormItem>
+          )}
+        ></FormField> */}
+        <FormField
+          control={form.control}
+          name="url"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  {...field}
+                  // onChange={() => watchForm(form.watch("url"))}
+                  placeholder={searchTypes[form.watch("searchType") as keyof typeof searchTypes].exampleString}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="w-full"
+        >
+          {loading ? "Loading…" : "Submit"}
+        </Button>
+      </form>
+    </Form>
   );
 }
